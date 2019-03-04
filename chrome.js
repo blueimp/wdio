a3757030ef6f4271ba10bdf050e9b407247c07c8
Sebastian Tschan <mail@blueimp.net>
2019-03-04 23:28:23 +0100

    Make wdio.conf.js a symlink to chrome.js.

--- chrome.js
+++ chrome.js
@@ -1,1 +1,33 @@
-wdio.conf.js
+exports.config = Object.assign({}, require('./hooks'), {
+  hostname: 'chromedriver',
+  path: '/',
+  capabilities: [
+    {
+      // Set maxInstances to 1 if screen recordings are enabled:
+      // maxInstances: 1,
+      browserName: 'chrome',
+      chromeOptions: {
+        // Disable headless mode if screen recordings are enabled:
+        args: ['--headless', '--window-size=1440,900']
+      }
+    }
+  ],
+  logLevel: 'warn',
+  reporters: ['spec'],
+  framework: 'mocha',
+  mochaOpts: {
+    timeout: 60000
+  },
+  specs: ['test/**/*.js'],
+  maximizeWindow: true,
+  screenshots: {
+    saveOnFail: true
+  },
+  videos: {
+    enabled: false
+  },
+  mailhog: {
+    host: 'mailhog'
+  },
+  baseUrl: 'http://example:8080'
+})
