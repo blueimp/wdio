5c2057fc82bb1c0ed95c29aa889bc537b3e0c8e0
Sebastian Tschan <mail@blueimp.net>
2019-03-27 13:41:40 +0100

    Combine form validation and sending mail test files.

--- test/form-validation.js
+++
@@ -1,8 +0,0 @@
-describe('Form validation', () => {
-  it('enforces required fields', () => {
-    browser.url('/')
-    $('#submit').click()
-    $('#result').waitForExist(500, true)
-    browser.getTitle().should.equal('Send mail')
-  })
-})
--- test/sending-mail.js
+++ test/sending-mail.js
@@ -2,6 +2,13 @@ const id = uuidv4()
 const recipient = id + '@example.org'
 
 describe('Sending mail', () => {
+  it('enforces required fields', () => {
+    browser.url('/')
+    $('#submit').click()
+    $('#result').waitForExist(500, true)
+    browser.getTitle().should.equal('Send mail')
+  })
+
   it('sends mail', () => {
     browser.url('/')
     browser.getTitle().should.equal('Send mail')
