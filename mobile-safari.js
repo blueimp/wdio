bb40c2cf5f11bfe5c8279c2b291aff7a273a11a8
Sebastian Tschan <mail@blueimp.net>
2019-12-30 21:14:38 +0900

    Use iPhone 11 and iOS 13.3 by default.

--- mobile-safari.js
+++ mobile-safari.js
@@ -14,8 +14,8 @@ exports.config = Object.assign({}, require('./chrome').config, {
       maxInstances: 1,
       browserName: 'safari',
       platformName: 'iOS',
-      platformVersion: process.env.PLATFORM_VERSION || '12.2',
-      deviceName: process.env.DEVICE_NAME || 'iPhone SE',
+      platformVersion: process.env.PLATFORM_VERSION || '13.3',
+      deviceName: process.env.DEVICE_NAME || 'iPhone 11',
       orientation
     }
   ],
